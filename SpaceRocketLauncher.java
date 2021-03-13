54b6758f7c443eb09e6fca4fa072422f2d4bd053
KrutkoOleksii <78808009+KrutkoOleksii@users.noreply.github.com>
2021-03-14 01:11:22 +0200

    Create SpaceRocketLauncher.java

---
+++ SpaceRocketLauncher.java
@@ -0,0 +1,46 @@
+public class SpaceRocketLauncher {
+    
+    private int bigRocketCount;
+
+    private int smallRocketCount;
+
+    public int getBigRocketCount() {
+        return bigRocketCount;
+    }
+
+    public void setBigRocketCount(int bigRocketCount) {
+        if(bigRocketCount < 0 || bigRocketCount > 100) {
+            return;
+        }
+        this.bigRocketCount = bigRocketCount;
+    }
+
+    public int getSmallRocketCount() {
+        return smallRocketCount;
+    }
+
+    public void setSmallRocketCount(int smallRocketCount) {
+        if(smallRocketCount < 0 || smallRocketCount > 100) {
+            return;
+        }
+        this.smallRocketCount = smallRocketCount;
+    }
+  
+    public void launchBigRocket() {
+        if (bigRocketCount > 0) {
+            bigRocketCount--;
+            System.out.println("Launch big rocket");
+        }
+    }
+  
+    public void launchSmallRocket() {
+        if (smallRocketCount > 0) {
+            smallRocketCount--;
+            System.out.println("Launch small rocket");
+        }
+    }
+  
+    public int getTotalPower() {     
+        return bigRocketCount*100 + smallRocketCount*50;
+    }
+}
