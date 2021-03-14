a5a3f17543b233c74edb31b91586c7dc895fa336
KrutkoOleksii <78808009+KrutkoOleksii@users.noreply.github.com>
2021-03-14 22:47:24 +0200

    Create Tanker.java

---
+++ Tanker.java
@@ -0,0 +1,30 @@
+class Tanker {
+    private String serialNumber;
+    
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+  
+    protected String getFuelType() {
+        return "A500";
+    }
+  
+    public void refuel(int amount) {
+        System.out.println("Add "+amount+" of "+getFuelType()+", tanker serial number is "+serialNumber);
+    }
+}
+
+class XFuelTanker extends Tanker{
+    public String getFuelType() {
+        return "XFuel";
+    }
+}
+
+class MX200Tanker extends Tanker{
+    public String getFuelType() {
+        return "MX200";
+    }
+}
